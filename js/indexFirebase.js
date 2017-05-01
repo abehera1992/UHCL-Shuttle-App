25c99e3f0358b195b0e28f295b926e2d0366d49f
abehera1992 <abehera1992@gmail.com>
2017-05-01 17:47:19 -0500

    fixes-v1

--- js/indexFirebase.js
+++ js/indexFirebase.js
@@ -1,6 +1,4 @@
 
-
-
 /*
 
 INITIALIZE
@@ -20,76 +18,22 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 //INITIALIZE FIREBASE WEB APP
   firebase.initializeApp(config);
+    
     var auth = firebase.auth();
     var db = firebase.database();
     var messaging = firebase.messaging();
     
-    messaging.onTokenRefresh(function() {
-    messaging.getToken().then(function(refreshedToken) {
-      console.log('Token refreshed.');
-      // Indicate that the new Instance ID token has not yet been sent to the
-      // app server.
-      setTokenSentToServer(false);
-      // Send Instance ID token to app server.
-      sendTokenToServer(refreshedToken);
-      // [START_EXCLUDE]
-      // Display new Instance ID token and clear UI of all previous messages.
-      resetUI();
-      // [END_EXCLUDE]
-    }).catch(function(err) {
-      console.log('Unable to retrieve refreshed token ', err);
-      showToken('Unable to retrieve refreshed token ', err);
-    });
-  });
-  // [END refresh_token]
-    
-            // Handle incoming messages. Called when:
-        // - a message is received while the app has focus
-        // - the user clicks on an app notification created by a sevice worker
-        //   `messaging.setBackgroundMessageHandler` handler.
-        messaging.onMessage(function(payload) {
-          console.log("Message received. ", payload);
-          // ...
-        });
+//    messaging.requestPermission().then(function() {
+//  console.log('Notification permission granted.');
+//        // TODO(developer): Retrieve an Instance ID token for use with FCM.
+//        // ...
+//    }).catch(function(err) {
+//    console.log('Unable to get permission to notify.', err);
+//    });
     
     
     
-    function resetUI() {
-    clearMessages();
-    showToken('loading...');
-    // [START get_token]
-    // Get Instance ID token. Initially this makes a network call, once retrieved
-    // subsequent calls to getToken will return from cache.
-    messaging.getToken().then(function(currentToken) {
-      if (currentToken) {
-        sendTokenToServer(currentToken);
-        updateUIForPushEnabled(currentToken);
-      } else {
-        // Show permission request.
-        console.log('No Instance ID token available. Request permission to generate one.');
-        // Show permission UI.
-        updateUIForPushPermissionRequired();
-        setTokenSentToServer(false);
-      }
-    }).catch(function(err) {
-      console.log('An error occurred while retrieving token. ', err);
-      showToken('Error retrieving Instance ID token. ', err);
-      setTokenSentToServer(false);
-    });
-  }
-  // [END get_token]
     
-    messaging.requestPermission().then(function() {
-      console.log('Notification permission granted.');
-      // TODO(developer): Retrieve an Instance ID token for use with FCM.
-      // ...
-    }).catch(function(err) {
-      console.log('Unable to get permission to notify.', err);
-    });
-
-                
-          
-      
 
 //REDEFINE DOCUMENT AS LOCAL DOC
     var doc = document;
@@ -190,7 +134,8 @@ var ackActionsDiv = doc.getElementById('ack-actions-div');
     
 //INDEX PAGE
     
- 
+ var indexSignupButton = doc.getElementById('index-signup');
+ var indexBackButton = doc.getElementById('index-back');
     
 
 
@@ -298,13 +243,10 @@ FIREBASE METHODS
         }
       
       verifiedUser = true;
-/*
- *firebase cloud messaging
-*/
-   
+
       
       if (uid == 'pmKvDiX6itaaWrYH9R9Ggf0FRlG2'){
-           
+        
            if(loginCard && logoutCard && noticeCard){
                 loginCard.style.display = "none";
                 logoutCard.style.display = "none";
@@ -317,7 +259,7 @@ FIREBASE METHODS
                     });  
                 }
             if(pwdUsersOnlyDiv){
-                if(provider == "password" && uid == 'pmKvDiX6itaaWrYH9R9Ggf0FRlG2'){
+                if(provider == "password"){
                     if(verifiedUser){
                             // display account update options
                             pwdUsersOnlyDiv.style.display = "inline";
@@ -343,7 +285,6 @@ FIREBASE METHODS
       } // pwdUsersOnlyDiv ENDS 
          
     } else {
-        
         
        if(loginCard && logoutCard && noticeCard){
                 loginCard.style.display = "none";
